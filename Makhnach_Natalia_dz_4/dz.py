7913363a7d6e0c174c781243a7c50ef198ef2ab8
nataliknv <nataliknv36@gmail.com>
2022-03-14 18:54:41 +0300

    add dz 4

---
+++ Makhnach_Natalia_dz_4/dz.py
@@ -0,0 +1,22 @@
+
+import requests
+
+
+def currency_rates(currency_code='', url='http://www.cbr.ru/scripts/XML_daily.asp'):
+
+    i = requests.get(url)
+
+    currency = i.text.split(currency_code)
+
+    if len(currency) != 1:
+        val = currency[1].split('</Value>')[0].split('<Value>')[1]
+
+        val = float(val.replace(',', '.'))
+
+        return(val)
+
+    return None
+
+
+
+print('USD = ',(currency_rates('USD')), 'EUR = ',(currency_rates('EUR')))
